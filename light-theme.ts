97e3577c4061d75347279045372b51d288df4cd4
Ruchi-14 <67096116+Ruchi-14@users.noreply.github.com>
2020-06-18 19:58:17 +0530

    theme

---
+++ light-theme.ts
@@ -0,0 +1,10 @@
+import { Theme } from './symbols';
+
+export const lightTheme: Theme = {
+  name: 'light',
+  properties: {
+    '--background': '#f6f7f9',
+    '--on-background': '#000',
+   
+  }
+};
